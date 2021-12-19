7464489819f59f2536e66d237ef187cf98772a68
Wanyea <wanyea.barbel@knights.ucf.edu>
2021-12-19 17:18:57 -0500

    Initial commit

---
+++ Adventour/AdventourApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventourApp.swift
+//  Adventour
+//
+//  Created by Lisa Barbel on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct AdventourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adventour/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adventour
+//
+//  Created by Lisa Barbel on 12/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
